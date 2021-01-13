332f92f038ce012f852f01291eb901e4fbc7a953
Martin Wilk <wilk-martin@t-online.de>
2021-01-13 19:51:13 +0100

    little bugfixes

--- score.py
+++ score.py
@@ -1,15 +1,20 @@
 import json
 import numpy as np
-import joblib
-from azureml.core.model import Model
-import azureml.train.automl
+import os
+from sklearn.externals import joblib
+
 
 def init():
     global model
-    model_path = Model.get_model_path('wine-quality-model')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'wine-quality-model.pkl')
     model = joblib.load(model_path)
 
-def run(raw_data):
-    data = np.array(json.loads(raw_data)['data'])
-    y_hat = model.predict(data)
-    return y_hat.tolist()
+def run(data):
+    try:
+        data = np.array(json.loads(data)["data"])
+        result = model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
