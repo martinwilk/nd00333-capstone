6868203cc4eceef2385d591b77605f322484a20f
Martin Wilk <wilk-martin@t-online.de>
2021-01-14 22:24:03 +0100

    changed score.py

--- score.py
+++ score.py
@@ -17,4 +17,4 @@ def run(raw_data):
     # Get a prediction from the model
     predictions = model.predict(data)
     # Return the predictions as any JSON serializable format
-    return {"result": pd.Series(predictions).replace({-1: "BAD", 0:"MEDIUM", 1: "GOOD"}).tolist()}
+    return json.dumps({"result": pd.Series(predictions).replace({-1: "BAD", 0:"MEDIUM", 1: "GOOD"}).tolist()})
